6bda218c0cc75fca264642401da0817a612e2516
Trent Howe <t-howe1@hotmail.com>
2023-05-14 19:12:44 -0400

    added api component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,15 +2,27 @@ import { useState, useEffect } from 'react';
 
 import { copy, linkIcon, loader, tick } from '../assets';
 
+import { useLazyGetSummaryQuery } from '../services/article';
+
 const Demo = () => {
 
   const [article, setArticle] = useState({
     url: '',
     summary: '',
-  })
+  });
+
+  const [getSummary, {error, isFetching }] = useLazyGetSummaryQuery();
 
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    const { data } = await getSummary({ articleUrl : article.url})
+  };
+
+  if(data?.summary) {
+    const newArticle = { ...article, summary: data.summary };
+
+    setArticle(newArticle);
+
+    console.log(newArticle);
   }
 
   return (
